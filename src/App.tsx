a04fb5cd315f79cea218dc9aedf4eadeb5402ef9
Stefan Wille <github@stefanwille.com>
2020-02-20 17:04:56 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,66 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import "./App.css";
-import { observer } from "mobx-react-lite";
-import { observable } from "mobx";
+import { observer, useLocalStore } from "mobx-react-lite";
+import { observable, configure, action } from "mobx";
+import "mobx-react-lite/optimizeForReactDom";
 
-class OrderPageStore {
+configure({ enforceActions: "always" });
+
+class RootStore {
   @observable
-  counter = 0;
+  masterData: string = "Hello";
 }
 
-const OrderPageStoreContext = React.createContext(new OrderPageStore());
-const OrderPageStoreProvider = OrderPageStoreContext.Provider;
-const useOrderPageStore = () => useContext(OrderPageStoreContext);
+class OrderPageStore {
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+  }
 
-const OrderPage: React.FC<{}> = () => {
-  const [orderPageStore] = useState<OrderPageStore>(() => new OrderPageStore());
+  rootStore: RootStore;
+
+  @observable
+  counter = 0;
+
+  @action.bound
+  increment() {
+    this.counter++;
+  }
+}
+
+const OrderPageStoreContext = React.createContext<OrderPageStore | null>(null);
+const OrderPageStoreProvider = OrderPageStoreContext.Provider;
+const useOrderPageStore = (): OrderPageStore => {
+  const localStore = useContext(OrderPageStoreContext);
+  if (!localStore) {
+    throw new Error("Missing local store");
+  }
+  return localStore;
+};
+
+const OrderPage: React.FC<{}> = observer(() => {
+  const rootStore = new RootStore();
+  const orderPageStore = useLocalStore(() => new OrderPageStore(rootStore));
   return (
     <OrderPageStoreProvider value={orderPageStore}>
       <div>
         <h1>OrderPage</h1>
 
+        <div>In OrderPage: {orderPageStore.counter}</div>
         <OrderPageCounter />
         <OrderPageButton />
       </div>
     </OrderPageStoreProvider>
   );
-};
+});
 
 const OrderPageCounter: React.FC<{}> = observer(() => {
-  const store = useOrderPageStore();
-  return <div>Counter: {store.counter}</div>;
+  const orderPageStore = useOrderPageStore();
+  return <div>In OrderPageCounter: {orderPageStore.counter}</div>;
 });
 
 const OrderPageButton: React.FC<{}> = () => {
-  const store = useOrderPageStore();
-  return (
-    <button
-      onClick={() => {
-        store.counter += 1;
-      }}
-    >
-      Add
-    </button>
-  );
+  const orderPageStore = useOrderPageStore();
+  return <button onClick={orderPageStore.increment}>Add</button>;
 };
 
 function App() {
