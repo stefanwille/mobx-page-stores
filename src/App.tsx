6ff7c52b8840d76ccef10fe49182ac39251ec7c4
Stefan Wille <github@stefanwille.com>
2020-02-20 16:26:22 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useState } from "react";
+import "./App.css";
+import { observer } from "mobx-react-lite";
+import { observable } from "mobx";
+
+class OrderPageStore {
+  @observable
+  counter = 0;
+}
+
+const OrderPageStoreContext = React.createContext(new OrderPageStore());
+const OrderPageStoreProvider = OrderPageStoreContext.Provider;
+const useOrderPageStore = () => useContext(OrderPageStoreContext);
+
+const OrderPage: React.FC<{}> = () => {
+  const [orderPageStore] = useState<OrderPageStore>(() => new OrderPageStore());
+  return (
+    <OrderPageStoreProvider value={orderPageStore}>
+      <div>
+        <h1>OrderPage</h1>
+
+        <OrderPageCounter />
+        <OrderPageButton />
+      </div>
+    </OrderPageStoreProvider>
+  );
+};
+
+const OrderPageCounter: React.FC<{}> = observer(() => {
+  const store = useOrderPageStore();
+  return <div>Counter: {store.counter}</div>;
+});
+
+const OrderPageButton: React.FC<{}> = () => {
+  const store = useOrderPageStore();
+  return (
+    <button
+      onClick={() => {
+        store.counter += 1;
+      }}
+    >
+      Add
+    </button>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <OrderPage />
       </header>
     </div>
   );
